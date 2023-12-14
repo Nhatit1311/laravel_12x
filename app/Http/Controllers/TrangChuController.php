859daa0ba960e898bb72e45aeee728767e1ac40b
Nh?t Nguy?n <duynhat2471@gmail.com>
2023-12-14 09:11:01 +0700

    laravel_12x

---
+++ app/Http/Controllers/TrangChuController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChuyenMuc;
+use App\Models\SanPham;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class TrangChuController extends Controller
+{
+    public function index()
+    {
+        $sanPham = SanPham::where('tinh_trang', 1)
+                          ->orderByDESC('id')
+                          ->take(6)
+                          ->get();
+
+
+        $chuyenMuc = ChuyenMuc::where('tinh_trang', 1)->get();
+        return view('user.page.homepage', compact('sanPham', 'chuyenMuc'));
+    }
+
+    public function data()
+    {
+        $data = ChuyenMuc::where('tinh_trang',1)->get();
+
+        return response()->json([
+            'data'     => $data,
+        ]);
+    }
+}
